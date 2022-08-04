d116cda52458ccd6e5c3f7ac1d8c7d490aef66b7
katsugen <c.seungwon@gmail.com>
2022-08-04 16:21:03 +0900

    Add a composable function for preview

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -28,3 +28,9 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
