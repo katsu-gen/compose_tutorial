a1b18bf789c19ae714e450b3a4415b91a871bc2a
katsugen <c.seungwon@gmail.com>
2022-08-04 16:42:12 +0900

    Add a image and Arrange layout

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +30,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column() {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
     }
 }
 
@@ -41,8 +60,7 @@ fun PreviewMessageCard() {
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
     Button(
-        onClick = { updateCount(count + 1) },
-        colors = ButtonDefaults.buttonColors(
+        onClick = { updateCount(count + 1) }, colors = ButtonDefaults.buttonColors(
             backgroundColor = if (count > 5) Color.Green else Color.White
         )
     ) {
@@ -55,12 +73,9 @@ fun Counter(count: Int, updateCount: (Int) -> Unit) {
 fun PreviewCounter() {
     val counterState = remember { mutableStateOf(0) }
 
-    Counter(
-        count = counterState.value,
-        updateCount = { newCount ->
-            counterState.value = newCount
-        }
-    )
+    Counter(count = counterState.value, updateCount = { newCount ->
+        counterState.value = newCount
+    })
 }
 
 data class Message(val author: String, val body: String)
